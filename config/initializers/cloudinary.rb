9564c8efb44d9dc4c949de419e514b0a37932de6
Miriam Tocino <miriam.tocino@gmail.com>
2016-08-12 11:30:55 +0200

    Add Cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dnwkjak4p'
+  config.api_key = '921253331833321'
+  config.api_secret = 'EiKx07jsNa1-_ZHbv-EMMKFgI_A'
+  config.cdn_subdomain = true
+end
+
